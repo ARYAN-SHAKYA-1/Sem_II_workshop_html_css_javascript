b8d5e2ff82b84737c7d33a2b40b4ad33e61d3201
aryan_shakya <shakyaaryan567@gmail.com>
2026-06-18 08:09:36 +0545

    day 15

---
+++ JS_day_11/index.js
@@ -0,0 +1,173 @@
+/*s
+console.log("hello world");
+var a = 10;
+let b = 20;
+var c = 30;
+console.log(a, b, c);
+a = 40;
+b = 50;
+c = 60;
+console.log(a, b, c);
+for (let i = 0; i < 5; i++) {
+  console.log(i + " hello");
+}
+var a = 10;
+var b = "hello World";
+var c = "false";
+var d = null;
+var e;
+console.log(typeof a, b, c, d, e);
+var a = 10;
+var b = 20;
+var sum = a + b;
+var diff = b - a;
+var product = a * b;
+var question = a / b;
+var exponential = a ** b;
+console.log(sum, diff, product, question, exponential);
+let temp = a;
+a = b;
+b = temp;
+console.log(a, b);
+a = a + b;
+b = a - b;
+a = a - b;
+console.log(a, b);
+console.log(a < b);
+console.log(a > b);
+console.log(a == b);
+console.log("hello world");
+var a = 10;
+let b = 20;
+var c = 30;
+console.log(a, b, c);
+a = 40;
+b = 50;
+c = 60;
+console.log(a, b, c);
+for (let i = 0; i < 5; i++) {
+  console.log(i + " hello");
+}
+var a = 10;
+var b = "hello World";
+var c = "false";
+var d = null;
+var e;
+console.log(typeof a, b, c, d, e);
+var a = 10;
+var b = 20;
+var sum = a + b;
+var diff = b - a;
+var product = a * b;
+var question = a / b;
+var exponential = a ** b;
+console.log(sum, diff, product, question, exponential);
+let temp = a;
+a = b;
+b = temp;
+console.log(a, b);
+a = a + b;
+b = a - b;
+a = a - b;
+console.log(a, b);
+console.log(a < b);
+console.log(a > b);
+console.log(a == b);
+console.log(a != b);
+a = "hello";
+b = "helloo";
+console.log(a == b);
+a.reverse();
+console.log(a);
+console.log(a != b);
+a = "hello";
+b = "helloo";
+console.log(a == b);
+a.reverse();
+console.log(a);
+var a = "Ram";
+console.log(a.split("").reverse().join(""));
+
+
+var a = "10";
+var b = 10;
+console.log(a === b);
+console.log(a == b);
+
+if (a > 18) {
+  console.log("you acna vote");
+} else {
+  console.log("you can not vote");
+}
+const array = ["ram", "shyam", "hari", "shubum", "kale"];
+for (let i = 0; i < 5; i++) {
+  console.log(array[i]);
+}
+console.log("arr", array);
+const sortArr = array.sort();
+console.log("sorted array", sortArr);
+
+array.push(10);
+console.log("push", array);
+const poppedArray = array.pop();
+console.log("popped array", array);
+
+const newArray = [1, 2, 3, 4, 5];
+const concat1 = array.concat(newArray);
+console.log("concat", concat1);
+
+const slicedArray = newArray.slice(1, 4);
+console.log("sliced array", slicedArray);
+
+function add(a, b) {
+  return a + b;
+}
+console.log(add(10, 20));
+const object = {
+  name: "aryan",
+  age: 19,
+  city: "kathmandu",
+};
+console.log(Object.keys(object));
+console.log(Object.values(object));
+
+console.log(object.name);
+console.log(object["name"]);
+
+for (let i = 0; i < Object.keys(object).length; i++) {
+  console.log(i);
+  console.log(Object.keys(object)[i]);
+  console.log(Object.values(object)[i]);
+
+  const key = Object.keys(object)[i];
+  const value = object[key];
+  console.log(key, value);
+}
+console.log(
+  `My name is ${object.name} and I am ${object.age} and I live in ${object.city}`,
+);
+
+const obj = [
+  {
+    name: "hehe",
+    age: 15,
+    city: "hello",
+  },
+  {
+    name: "hehe",
+    age: 15,
+    city: "hello",
+  },
+  {
+    name: "hehe",
+    age: 15,
+    city: "hello",
+  },
+  {
+    name: "hehe",
+    age: 15,
+    city: "hello",
+  },
+];
+const newSet = new Set(1, 2, 3, 4, 5, 6, 7, 8, 9);
+*/
